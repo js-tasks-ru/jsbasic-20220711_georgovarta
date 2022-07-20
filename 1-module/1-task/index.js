833ea1aa050dd26c59d7b62f744a8be4b30e93b6
Georgii Vartanov <georgovarta@gmail.com>
2022-07-20 21:28:56 +0200

    Решение задачи по фактариалу

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,27 @@
-function factorial(n) {
-  // ваш код...
-}
+
+  function factorial(n) {
+
+    if (n === 0){
+ 
+      return n + 1;     // Проверка N != 0
+ 
+    } else if (n === 1){
+ 
+      return n;     // Проверка N != 1
+ 
+    }else {
+ 
+      for (let i = 0; i <= n; i++) {
+ 
+        return n * factorial(n - 1) / 1;
+ 
+     }
+ 
+ 
+ 
+    }
+    
+   
+ 
+ }
+
